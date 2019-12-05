2c415f7a0e616dfa0d17ec3fe29f6771a708613f
unknown <shi707cn@163.com>
2019-12-05 13:29:29 +0800

    first commit

---
+++ server/db.js
@@ -0,0 +1,10 @@
+// 数据库连接配置
+module.exports = {
+  mysql: {
+    host: '119.3.243.116',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    port: '49.4.64.51'
+  }
+}
---
+++ server/sqlMap.js
@@ -0,0 +1,8 @@
+// sql语句
+var sqlMap = {
+  // 用户
+  user: {
+    add: 'insert into user(id, name, age) values (2, ?, ?)'
+  }
+}
+module.exports = sqlMap
